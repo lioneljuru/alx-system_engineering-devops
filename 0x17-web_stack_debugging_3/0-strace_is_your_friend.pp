05e141524d910c3bb35757053e53d8e1418fc89c
lioneljuru <136335805+lioneljuru@users.noreply.github.com>
2024-05-10 00:31:19 +0300

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # atomated puppet fix (Correct internal server error)
 
-excec { 'replace':
+exec {'replace':
   provider => shell,
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
